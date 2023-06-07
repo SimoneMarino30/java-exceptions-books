6305ebc434e525309faa2461a35299caec8cd0d4
Simone Marino <simo.marino@outlook.it>
2023-06-07 17:01:35 +0200

    Book's array can be created

--- src/org/lessons/java/books/Book.java
+++ src/org/lessons/java/books/Book.java
@@ -6,10 +6,88 @@ public class Book {
     private String author;
     private String editor;
 
-    public Book(String title, int pagesNumber, String author, String editor) {
-        this.title = title;
+    // CONSTRUCTOR
+    public Book(String title, int pagesNumber, String author, String editor) throws RuntimeException {
+        if(title != null && !title.isBlank()) {
+            this.title = title;
+        } else {
+            throw new RuntimeException("You've inserted a wrong value, please be sure that: \n - title must be a valid string");
+        }
+        if(pagesNumber > 0) {
+            this.pagesNumber = pagesNumber;
+        } else {
+            throw new RuntimeException("You've inserted a wrong value, please be sure that: \n - pages must be a number greater than 0");
+        }
+        if(author != null && !author.isBlank()) {
+            this.author = author;
+        } else {
+            throw new RuntimeException("You've inserted a wrong value, please be sure that: \n - author must be a valid string");
+        }
+        if(editor != null && !editor.isBlank()) {
+            this.editor = editor;
+        } else {
+            throw new RuntimeException("You've inserted a wrong value, please be sure that: \n - editor must be a valid string");
+        }
+    }
+
+    // GETTERS & SETTERS
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        if(title != null && !title.isBlank()) {
+            this.title = title;
+        } else {
+            throw new RuntimeException("You've inserted a wrong value, please be sure that: \n - title must be a valid string");
+        }
+    }
+
+
+    public int getPagesNumber() {
+        return pagesNumber;
+    }
+
+    public void setPagesNumber(int pagesNumber) {
         this.pagesNumber = pagesNumber;
-        this.author = author;
-        this.editor = editor;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        if(author != null && !author.isBlank()) {
+            this.author = author;
+        } else {
+            throw new RuntimeException("You've inserted a wrong value, please be sure that: \n - author must be a valid string");
+        }
+    }
+
+    public String getEditor() {
+        return editor;
+    }
+
+    public void setEditor(String editor) {
+        if(editor != null && !editor.isBlank()) {
+            this.editor = editor;
+        } else {
+            throw new RuntimeException("You've inserted a wrong value, please be sure that: \n - author must be a valid string");
+        }
+    }
+
+    // METHODS
+
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", pagesNumber=" + pagesNumber +
+                ", author='" + author + '\'' +
+                ", editor='" + editor + '\'' +
+                '}';
     }
 }
+
+
