380912809e973504e483608314f8af0b05076db0
Simone Marino <simo.marino@outlook.it>
2023-06-07 13:54:09 +0200

    Init

---
+++ src/org/lessons/java/books/Book.java
@@ -0,0 +1,15 @@
+package org.lessons.java.books;
+
+public class Book {
+    private String title;
+    private int pagesNumber;
+    private String author;
+    private String editor;
+
+    public Book(String title, int pagesNumber, String author, String editor) {
+        this.title = title;
+        this.pagesNumber = pagesNumber;
+        this.author = author;
+        this.editor = editor;
+    }
+}
